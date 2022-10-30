d58cd30d9de02e270f277bd803e41be20f1f93e4
DaltonCody-FS <94862477+DaltonCody-FS@users.noreply.github.com>
2022-10-30 14:39:44 -0400

    Three GET routes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 
-// GET / HTTP/1.1
-app.post('/', (req, res) => {
-    res.status(200),json({
-        "first-name":"Cody",
-        "last-name":"Dalton",
-        "email":"cddalton@student.fullsail.edu",
-        "request-token":"ASL-442"
-    })
+//GET all products - by page, sort, and order parameters
+app.get("/products/all/:page-:sort-:order", (req, res) => {
+    res.send(
+        "GET Products: "+req.params.page+","+req.params.sort+","+req.params.order
+        )
 });
 
+//GET product - by id, size, and color parameters
+app.get("/products/:id-:size-:color", (req, res) => {
+    res.send(
+        "GET Products: "+req.params.id+","+req.params.size+","+req.params.color
+        )
+});
+
+//GET product - by id
+app.get('/products/:id', (req, res) => {
+    res.send(
+        "GET Products: "+req.params.id
+    )
+});
+
+//Listen on port 3000
 app.listen(3000)
